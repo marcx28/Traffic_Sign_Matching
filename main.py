d0bf0b1895b0c26ed06834658661b80f33a215c2
marcx28 <ha.marco14@gmail.com>
2019-04-06 11:46:38 +0200

    Implement Edge Detection

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 import cv2
+import numpy as np
+from matplotlib import pyplot as plt
 
-capture = cv2.VideoCapture(0)
+capture = cv2.VideoCapture('Autofahrt.mp4')
 print("hello world")
 while cv2.waitKey(1) != ord('q'):
-    ret, frame = capture.read()
-    cv2.imshow('frame', frame)
+    _, frame = capture.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lower_red = np.array([30, 150, 50])
+    upper_red = np.array([255, 255, 180])
+
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+    res = cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.imshow('Original', frame)
+    edges = cv2.Canny(frame, 100, 200)
+    cv2.imshow('Edges', edges)
+
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+
+cv2.destroyAllWindows()
+capture.release()
 
