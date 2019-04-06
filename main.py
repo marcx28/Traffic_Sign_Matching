1458d63a4f38c91e4ee24cbecb4a1000c3488986
bermac16 <bermac16@htl-kaindorf.at>
2019-04-06 10:15:18 +0200

    show video capture

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello world");
+import cv2
+
+capture = cv2.VideoCapture(0)
+print("hello world")
+while cv2.waitKey(1) != ord('q'):
+    ret, frame = capture.read()
+    cv2.imshow('frame', frame)
+
